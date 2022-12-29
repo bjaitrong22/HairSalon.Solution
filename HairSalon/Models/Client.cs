f039868af8972c2cec4f2bd5227b5f68fda6a6fa
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-28 19:50:19 -0700

    Add database model design, Stylist and Client class.

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,13 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Description { get; set;}
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set;}
+  }
+}
