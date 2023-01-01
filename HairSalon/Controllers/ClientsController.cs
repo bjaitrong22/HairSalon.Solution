218bac561de0ed2439f9cdf57fdb42003cbdef75
Bai Jaitrong <bjaitrong@comcast.net>
2022-12-31 21:54:47 -0700

    Add Edit route, view, and update link.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -57,5 +57,29 @@ namespace HairSalon.Controllers
       Client thisClient = _db.Clients.Include(client => client.Stylist).FirstOrDefault(client => client.ClientId == id);
       return View(thisClient);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId","LastName");
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      if (client.FirstName != null && client.LastName != null && client.PhoneNumber != null && client.Description != null)
+      {
+        _db.Clients.Update(client);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "FirstName");
+        ViewBag.ErrorMessage = "Please fill in all fields!";
+        return View();
+      }    
+    }
   }
 }
